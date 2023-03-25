6708db8198c1b11dfad60b9ad5ca1dbcb2f43c0f
samgcode <samgcode@gmail.com>
2023-03-24 21:08:09 -0600

    rewrite image converter to use promises and file stream
    - holy shit this took me 3 hours why is it so hard to read a file stream synchronously

--- imgLoader.js
+++ imgLoader.js
@@ -1,53 +1,24 @@
-const { jsonUtility, Level } = require('rptd-core') 
-const PNG = require('png-js')
+const { jsonUtility, Level } = require('rptd-core')
+const fs = require('fs')
+const { PNG } = require('pngjs')
 
 //a list of all the tiles that can be selected from format is tileid, r,g,b values
 const hotTiles = require('./json/hotTiles.json')
 const coldTiles = require('./json/coldTiles.json')
 
-function createImg(path = './images/testImg.png', level, finish){
-    let img = PNG.load(path)
-    console.log("image w",img.width,", h",img.height)
-
-    
-    img.decodePixels(function(pixels){
-        console.log("len"+pixels.length)
-        for(let y=0;y<img.height;y++){
-            for(let x=0;x<img.width;x++){
-                let tileInfo = pixelToTile(x,y,img,pixels)
-                let tile = tileInfo[0]
-                let hot = tileInfo[1]
-
-                //we gotta flip the y axis
-                if(hot){
-                    level.addTile({ ID: tile, x:x, y:-y})
-                }else{
-                    level.addTile({ ID: tile, x:x, y:-y+1000})
-                }
-            }
-        }
-        finish()
-    })
-
-}
-
 // returns tile id and whether the tile will be hot or cold
-function pixelToTile(x ,y,img,pixels){
-    let valuesperPixel = pixels.length/(img.width*img.height)//why do i need this
+function pixelToTile(x ,y, pixel){
     //rgb values of the pixel
-    let r = pixels[(y*img.width+x)*valuesperPixel]
-    let g = pixels[(y*img.width+x)*valuesperPixel+1]
-    let b = pixels[(y*img.width+x)*valuesperPixel+2]
+    const { r, g, b } = pixel
     //difference from actual pixel to tile colour
     let diff = 255*3
     let tile = -1 // index of the tile 
-    let hot = true
+    let type = 0
 
     for(let i in hotTiles){
-        
-        let rDiff = Math.abs(r-hotTiles[i][1]) 
-        let gDiff = Math.abs(g-hotTiles[i][2])
-        let bDiff = Math.abs(b-hotTiles[i][3])
+        const rDiff = Math.abs(r-hotTiles[i][1]) 
+        const gDiff = Math.abs(g-hotTiles[i][2])
+        const bDiff = Math.abs(b-hotTiles[i][3])
 
         if(diff>rDiff+gDiff+bDiff){
             tile = hotTiles[i][0]
@@ -55,22 +26,67 @@ function pixelToTile(x ,y,img,pixels){
         }
     }
     for(let i in coldTiles){
-        
-        let rDiff = Math.abs(r-coldTiles[i][1]) 
-        let gDiff = Math.abs(g-coldTiles[i][2])
-        let bDiff = Math.abs(b-coldTiles[i][3])
+        const rDiff = Math.abs(r-coldTiles[i][1]) 
+        const gDiff = Math.abs(g-coldTiles[i][2])
+        const bDiff = Math.abs(b-coldTiles[i][3])
 
         if(diff>rDiff+gDiff+bDiff){
             tile = coldTiles[i][0]
             diff = rDiff+gDiff+bDiff
-            hot = false
+            type = 1
         }
     }
     if(tile ===-1){
        console.log("something went wrong. pixel at "+x+","+y+" is -1 with r:"+r+" g:"+g+" b:"+b+" values per pixel:"+valuesperPixel )
     }
 
-    return [tile,hot]
+    return { tile, type }
 }
 
-module.exports = {createImg,hotTiles,coldTiles}
+async function convertImage(level, filepath) {
+    const promise = new Promise(function(resolve, error) {
+        const stream = fs.createReadStream(filepath).on('end', () => {
+            resolve()
+        }).pipe(
+            new PNG({
+                filterType: 4,
+            })
+        )
+        .on('parsed', function () {
+            for (let y = 0; y < this.height; y++) {
+                for (let x = 0; x < this.width; x++) {
+                    let idx = (this.width * y + x) << 2;
+            
+                    const r = this.data[idx]
+                    const g = this.data[idx + 1]
+                    const b = this.data[idx + 2]
+            
+                    let tileData = pixelToTile(x, y, { r, g, b });
+
+                    switch(tileData.type) {
+                        case 0:
+                            level.addTile({ ID: tileData.tile, x: x, y: -y });
+                            break;
+                        case 1:
+                            level.addTile({ ID: tileData.tile, x: x, y: -y+1000 });
+                            break;
+                    }
+                }
+            }
+        })
+    })
+    
+    await promise
+
+    const levelObj = { ...level }
+
+    delete level.tileCount
+    delete level.prefabCount
+    delete level.gateCount
+    delete level.channelsUsed
+
+    const levelString = JSON.stringify(level)
+    return { levelObj, levelString }
+}
+
+module.exports = { convertImage }
--- test.js
+++ test.js
@@ -4,11 +4,12 @@ const img = require('./imgLoader')
 const level = new Level({LevelName: "Image",Creator:"", Description:"Image"}) 
 level.createSection({Name:"Image",LevelBounds:{x:0,y:60,z:1,w:-60},spawnPointY:1})
 
-img.createImg('./images/omega.png', level, function(){
-        
-    //outputting file
-    jsonUtility.writeLevelToFile(level, './output.json')
-    console.log("outputed file")
-})
+const fileName = 'omega.png'
 
+async function createImage() {
+    const data = await img.convertImage(level, `./images/${fileName}`)
+    console.log(data.levelString)
+    jsonUtility.writeLevelToFile(data.levelObj, './output.json')
+}
 
+createImage()
